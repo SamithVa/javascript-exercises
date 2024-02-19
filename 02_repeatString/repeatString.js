71a0b3213efcbc0ff44096e9b0f07f4c9e8acabd
samithva <vasamith12@gmail.com>
2024-02-20 01:56:52 +0800

    01-03 exercises -  solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, times) {
+    let result = ''
+    if(times < 0){
+        return "ERROR"
+    }
+    else {
+        for(let i = 0; i <times; i++){
+            result += str
+        }
+        return result
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(str) {
 
+    // split into an array
+    let splitStr = str.split('')
+    // reverse the order of array
+    splitStr = splitStr.reverse()
+
+    // join the array back into result string
+    return splitStr.join('')
 };
 
 // Do not edit below this line
